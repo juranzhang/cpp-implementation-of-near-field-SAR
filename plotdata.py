516e060e559a906576c273ad6f27bbc9658957bb
juran <zhangjurann@gmail.com>
2016-11-05 18:15:33 -0700

    simple simulation

--- plotdata.py
+++ plotdata.py
@@ -2,7 +2,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('example.txt')
+data = np.loadtxt('image_r_x_mat.txt')
 # data = np.random.random((408, 1600))
 plt.imshow(data, cmap='hot', interpolation='nearest')
 plt.show()
