407e8ad41f31ed024d561209964adb975455313a
juran <zhangjurann@gmail.com>
2017-01-10 10:30:55 -0800

    mex version of stolt interrupt

---
+++ stolt_mex.cpp
@@ -0,0 +1,196 @@
+#define _USE_MATH_DEFINES
+#include "mex.h"
+#include <math.h> 
+using namespace std;
+
+mxArray* sinc(mxArray *x){
+	int M = mxGetM(x);
+	int N = mxGetN(x);
+	mxArray *res = mxCreateNumericMatrix(M,N,mxDOUBLE_CLASS,mxREAL);
+	for(int i=0;i<M*N;i++){
+		if(x[i] == 0){
+			res[i] = 1;
+		}
+		else{
+			res[i] = sin(M_PI * x[i]) / (M_PI * x[i]);
+		}
+	}
+	return res;
+}
+/*
+	3D stolt interrupt
+*/
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+	#define Stolt_OUT plhs[0]
+	#define S_matched_IN prhs[0]
+	#define k_IN prhs[1]
+	#define kx_IN prhs[2]
+	#define ky_IN prhs[3]
+	#define kz_interp_IN prhs[4]
+	#define deltkr_IN prhs[5]
+	#define Nx_IN prhs[6]
+	#define Ny_IN prhs[7]
+	#define Nf_IN prhs[8]
+	#define kz_dim_IN prhs[9]
+	#define kx_max_IN prhs[10]
+	#define ky_max_IN prhs[11]
+	#define p_IN prhs[12]
+	
+	double *S_matchedr = mxGetPr(S_matched_IN);
+	double *S_matchedi = mxGetPi(S_matched_IN);
+	size_t K = mxGetNumberOfDimensions(S_matched_IN);
+	const mwSize *Ni = mxGetDimensions(S_matched_IN);
+	// create identity cube
+	const mwSize *N;
+	N[0] = Ny_IN;
+	N[1] = Nx_IN;
+	N[2] = kz_dim_IN;
+	double *k = mxGetPr(k_IN);
+	mxArray *identity = mxCreateNumericArray(K, N, mxDOUBLE_CLASS, mxREAL);
+	for(int i=0;i<K;i++){
+		identity[i] = k[0];
+	}
+
+	// create kz_interp cube
+	double *kz_interp = mxGetPr(kz_interp_IN);
+	mxArray *kz_interp_cub = mxCreateNumericArray(K, N, mxDOUBLE_CLASS, mxREAL);
+	for(int i=0;i<N[0]*N[1]*N[2];i++){
+		kz_interp_cub[i] = kz_interp[i%N[2]];
+	}
+
+	// create kx cube
+	double *kx = mxGetPr(kx_IN);
+	mxArray *kx_cub = mxCreateNumericArray(K, N, mxDOUBLE_CLASS, mxREAL);
+	for(int i=0;i<N[0]*N[1]*N[2];i++){
+		kx_cub[i] = kx[i%N[1]];
+	}
+
+	// create ky cube
+	double *ky = mxGetPr(ky_IN);
+	mxArray *ky_cub = mxCreateNumericArray(K, N, mxDOUBLE_CLASS, mxREAL);
+	for(int i=0;i<N[0]*N[1]*N[2];i++){
+		ky_cub[i] = ky[i%N[0]];
+	}
+
+	// create DKZ
+	mxArray *DKZ = mxDuplicateArray(identity);
+	for(int i=0;i<N[0]*N[1]*N[2];i++){
+		DKZ[i] = 0.5*sqrt(pow(kx_cub,2)+pow(ky_cub,2)+pow(kz_interp,2)) - identity[i];
+	}
+
+	// create NDKZ, find min and max at the same time
+	mxArray *NDKZ = mxDuplicateArray(identity);
+	double NDKZ_min = floor(DKZ[0]/deltkr_IN);
+	double NDKZ_max = NDKZ_min;
+	for(int i=0;i<N[0]*N[1]*N[2];i++){
+		NDKZ[i] = floor(DKZ[i]/deltkr_IN);
+		if(NDKZ[i] < NDKZ_min){
+			NDKZ_min = NDKZ[i];
+		}
+		if(NDKZ[i] > NDKZ_max){
+			NDKZ_max = NDKZ[i];
+		}
+	}
+	
+	// create B1
+	const mwSize *N_B1;
+	N_B1[0] = Ny;
+	N_B1[1] = Nx;
+	N_B1[2] = NDKZ_max - NDKZ_min + p_IN + 1;
+	mxArray *B1 = mxCreateNumericArray(K, N_B1, mxDOUBLE_CLASS, mxCOMPLEX);
+	double *B1r = mxGetPr(B1);
+	double *B1i = mxGetPi(B1);
+	for(int i=0;i<N_B1[0]*N_B1[1]*N_B1[2];i++){
+		B1r[i] = 0;
+		B1i[i] = 0;
+	}
+
+	for(int i=0;i<Ni[0]*Ni[1]*Ni[2];i++){
+		B1r[i+(-NDKZ_min)*Ni[0]*Ni[1]] = S_matchedr[i];
+		B1i[i+(-NDKZ_min)*Ni[0]*Ni[1]] = S_matchedi[i];
+	}
+
+	// preprocessing
+	mxArray *win_interp = mxCreateNumericMatrix(1,2*p_IN,mxDOUBLE_CLASS, mxREAL);
+	for(uword i=0;i<2*p_IN;i++){
+		win_interp[i] = 0.53836 - 0.46164*cos(2*M_PI*i/(2*p_IN-1));
+	}
+	mxArray *NN = mxCreateNumericMatrix(1,2*p_IN,mxDOUBLE_CLASS,mxREAL);
+	mxArray *be4sinc = mxCreateNumericMatrix(1,2*p_IN,mxDOUBLE_CLASS,mxREAL);
+	mxArray *B2 = mxCreateNumericMatrix(2*p_IN,1,mxDOUBLE_CLASS,mxCOMPLEX);
+	double *B2r = mxGetPr(B2);
+	double *B2i = mxGetPi(B2);
+	
+	mxArray *Stolt_OUT = mxCreateNumericArray(K, N, mxDOUBLE_CLASS, mxCOMPLEX);
+	double *Stolt_OUTr = mxGetPr(Stolt_OUT);
+	double *Stolt_OUTi = mxGetPi(Stolt_OUT);
+	for(int i=0;i<Ny_IN;i++){
+		for(int j=0;j<Nx_IN;j++){
+			for(int nn=0;nn<2*p_IN;nn++){
+				NN[nn] = 0;
+				be4sinc[nn] = 0;
+				B2r[nn] = 0;
+				B2i[nn] = 0;
+			}
+			for(int q = p_IN -1;q<kz_dim_IN;q++){
+				for(int k=0;k<2*p_IN;k++){
+					NN[k] = NDKZ[i + N[0]*(j + N[1]*q)]+k+1-p_IN;
+					be4sinc[k] = DKZ[i+ N[0]*(j + N[1]*q)]/deltkr_IN - NN[k];
+					B2r[k] = B1r[i + N_B1[0]*(j + N_B1[1]*(NN[k]-NDKZ_min))];
+					B2i[k] = B1i[i + N_B1[0]*(j + N_B1[1]*(NN[k]-NDKZ_min))];
+				}
+
+				// Stolt(i,j,q)=win_interp .* sinc( DKZ(i,j,q)/deltkr - NN )*B2; 
+				mxArray *be4stolt = mxCreateNumericMatrix(1,2*p_IN,mxDOUBLE_CLASS,mxCOMPLEX);
+				double *be4stoltr = mxGetPr(be4stolt);
+				double *be4stolti = mxGetPi(be4stolt);
+				for(int i=0;i<2*p_IN;i++){
+					be4stoltr[i] = win_interp[i] * sinc(be4sinc);
+					be4stolti[i] = 0;
+				}
+				double sumr = 0;
+				double sumi = 0;
+				for(int i=0;i<2*p_IN;i++){
+					sumr += be4stoltr[i]*B2r[i];
+					sumi += be4stolti[i]*B2i[i];
+				}
+				Stolt_OUTr[i+ N[0]*(j + N[1]*q)] = sumr;
+				Stolt_OUTi[i+ N[0]*(j + N[1]*q)] = sumi;
+
+				if(!((abs(kx_cub[i+ N[0]*(j + N[1]*q)]) < kx_max_IN) && (ky_cub[i+ N[0]*(j + N[1]*q)] < ky_max_IN))) {
+					Stolt_OUTr[i+ N[0]*(j + N[1]*q)] = 0;
+					Stolt_OUTi[i+ N[0]*(j + N[1]*q)] = 0;
+				}
+			}
+		}
+	}
+
+
+
+	for(uword i=0;i<Ny;i++) {
+		for(uword j=0;j<Nx;j++){
+			NN.zeros();
+			be4sinc.zeros();
+			B2.zeros();
+			for(uword q=p-1;q<kz_dim;q++){
+				for(uword k=0;k<NN.n_elem;k++){
+					NN(0,k) = NDKZ(i,j,q)+k+1-p;
+					be4sinc(0,k) = DKZ(i,j,q)/deltkr - NN(0,k);
+					B2(k,0) = B1(i,j,NN(0,k)-NDKZ_min);
+				}
+				mat be4stolt_real = win_interp % sinc(be4sinc);
+				mat be4stolt_imag(size(be4stolt_real), fill::zeros);
+				cx_mat be4stolt(be4stolt_real,be4stolt_imag);
+				Stolt(i,j,q) = mat2cx_double(be4stolt * B2);
+
+				if(!((abs(kx_cub(i,j,q)) < kx_max_IN) && (ky_cub(i,j,q) < ky_max_IN))) {
+					Stolt(i,j,q).real(0.0);
+					Stolt(i,j,q).imag(0.0);
+				}
+			}
+			
+		}
+	}
+	
+}
